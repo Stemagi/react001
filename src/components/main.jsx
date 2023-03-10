461e109fbc17bb98b4e67641de2ace42c9d43d16
stm <stm2485@gmail.com>
2023-03-10 10:42:26 +0200

    neskolko popravok i gotovo

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -5,11 +5,11 @@ export default function Main() {
     <div className="main">
       <h1 className="main-heading">Fun Facts about React</h1>
       <ul className="main-content">
-      <li className="facts"></li>
-      <li className="facts"></li>
-      <li className="facts"></li>
-      <li className="facts"></li>
-      <li className="facts"></li>
+      <li className="facts">Was first released in 2013</li>
+      <li className="facts">Was originally created by Jordan Walke</li>
+      <li className="facts">Has well over 100K stars on GitHub</li>
+      <li className="facts">Is maintained by Facebook</li>
+      <li className="facts">Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
     </div>
   )
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return(
     <div className="navbar">
-      <img src="" alt="" />
+      <img src="../images/react-logo.png" />
       <h3 className="heading">ReactFacts</h3>
       <h4 className="semi-heading">React Course-First project</h4>
     </div>
