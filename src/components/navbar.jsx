2f8e8d3ecc68533484070173e95a91222be6c3f8
stm <stm2485@gmail.com>
2023-03-11 10:49:45 +0200

    finished)))

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return(
     <div className="navbar">
-      <img src="../../public/react-logo.png" alt="react"/>
+      <img src="react-logo.png" alt="react" className="image"/>
       <h3 className="heading">ReactFacts</h3>
       <h4 className="semi-heading">React Course-First project</h4>
     </div>
