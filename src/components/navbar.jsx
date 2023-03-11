9839fd8e6fabb4e1e590100fbcb71c475769d09b
stm <stm2485@gmail.com>
2023-03-11 10:22:42 +0200

    moved img in public

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return(
     <div className="navbar">
-      <img src="../images/react-logo.png" alt="react"/>
+      <img src="../../public/react-logo.png" alt="react"/>
       <h3 className="heading">ReactFacts</h3>
       <h4 className="semi-heading">React Course-First project</h4>
     </div>
